38bcaba39c7f8266132095cda22004444bc99bf6
ghost <fayyad_r22@yahoo.com>
2019-01-22 20:02:52 +0100

    add Todos component and render in app vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
   <div id="app">
 
-
+<Todos />
   </div>
 </template>
 
 <script>
-
+import Todos from './components/Todos'
 export default {
   name: 'app',
   components: {
-    
+Todos
+  },
+  data(){
+    return{
+    todos:[
+      {
+        id:1,
+        title:'Todo One',
+        completed:false
+      },
+      {
+        id:2,
+        title:'Todo Tow',
+        completed:false
+      },
+      {
+        id:3,
+        title:'Todo Three',
+        completed:false
+      },
+    ]
+    }
   }
 }
 </script>
