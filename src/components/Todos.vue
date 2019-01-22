9c713bab8f8bacb34458687068df165701f94a42
ghost <fayyad_r22@yahoo.com>
2019-01-22 20:52:11 +0100

    add delet event to delet todo item

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -3,7 +3,7 @@
   <div >
 
   <div v-bind:key="todo.id" v-for="todo in todos">
-    <TodoItem v-bind:todo="todo"/>
+    <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
   </div>
 
   </div>
