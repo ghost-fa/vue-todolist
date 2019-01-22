db4e89eccee4831e0b8b2a8a4fb21541b14e6215
ghost <fayyad_r22@yahoo.com>
2019-01-22 20:18:30 +0100

    add todoitem componente and pass the props and render todo title

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -3,7 +3,7 @@
   <div >
 
   <div v-bind:key="todo.id" v-for="todo in todos">
-    <h3>{{todo.title}}</h3>
+    <TodoItem v-bind:todo="todo"/>
   </div>
 
   </div>
@@ -11,8 +11,12 @@
 </template>
 
 <script>
+import TodoItem from './TodoItem.vue'
 export default{
   name:'Todos',
+  components:{
+    TodoItem
+  },
   props:['todos']
 }
 
